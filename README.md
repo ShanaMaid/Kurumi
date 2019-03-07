8ef936d55606d721374ca1056383935c6fbdce94
shanamaid <uestczeng@gmail.com>
2019-03-07 16:52:45 +0800

    refactor:  rewrite Kurumi

--- README.md
+++ README.md
@@ -5,35 +5,27 @@
   </a>
   <img src='https://img.shields.io/npm/v/kurumi.svg?style=flat-square' alt="version">
   <img src='https://img.shields.io/npm/l/kurumi.svg' alt="license">
-  <img src='http://img.badgesize.io/https://unpkg.com/kurumi/lib/Archer.js?compression=gzip&label=gzip%20size:%20&style=flat-square'>
+  <img src='http://img.badgesize.io/https://unpkg.com/kurumi/lib/Kurumi.js?compression=gzip&label=gzip%20size:%20&style=flat-square'>
   <img src='https://img.shields.io/npm/dt/kurumi.svg?style=flat-square' alt="downloads">
   <img src='https://img.shields.io/npm/dm/kurumi.svg?style=flat-square' alt="downloads-month">
 </p>
 
 > if you think that `Kurumi` can help you，please `star` or `follow` to support me! 😘
 
-<div align=center>
-
-![](./docs/show.gif)
-
-</div>
-
 ## Introduction
 -  Install npm dependencies that run directly in the browser. No Browserify, Webpack or import maps required.
 - Imports source code URLs(it must support `UMD`)! most of all, It don't affect global variable as same as `webpack`
 
-You can use `await`
 ```js
-const myLodash = await Kurumi('lodash', 'https://unpkg.com/lodash@4.17.11/lodash.min.js');
+const kurumi = new Kurumi({
+  modules: {
+  'lodash': 'https://unpkg.com/lodash@4.17.11/lodash.min.js'
+  }
+});
+await kurumi.install();
+const myLodash = kurumi.get('lodash');
 console.log(myLodash.template(`Wow! so cool! <%= name %>`)({ name: 'Kurumi!!!' })); // Wow! so cool! Kurumi!!!
 console.log(lodash) // undefined
-```
-`or` `.then`
-```js
-Kurumi('lodash','https://unpkg.com/lodash@4.17.11/lodash.min.js')
-.then((lodash) => {
-  console.log(lodash.template(`Wow! so cool! <%= name %>`)({ name: 'Kurumi!!!' }));
-})
 ```
 - No more `node_modules`, no dependency to install.
 
@@ -44,25 +36,60 @@ npm install kurumi
 
 ## Usage
 ### Webpack
-```js
-import Kurumi from 'kurumi';
-Kurumi('lodash', 'https://unpkg.com/lodash@4.17.11/lodash.min.js')
-.then((lodash) => {
-  console.log(lodash.template(`Wow! so cool! <%= name %>`)({ name: 'Kurumi!!!' }));
-})
-```
+use kurumi in React, [Onlie Demo](https://stackblitz.com/edit/react-1jql5r);
 
 ### Browser
-```
+```html
 <script src="https://unpkg.com/kurumi/lib/Kurumi.min.js"></script>
 <script>
-Kurumi('lodash', 'https://unpkg.com/lodash@4.17.11/lodash.min.js')
-.then((lodash) => {
-  console.log(lodash.template(`Wow! so cool! <%= name %>`)({ name: 'Kurumi!!!' }));
-})
+(async () => {
+  const kurumi = new Kurumi({
+    modules: {
+    'lodash': 'https://unpkg.com/lodash@4.17.11/lodash.min.js'
+    }
+  });
+  await kurumi.install();
+  const myLodash = kurumi.get('lodash');
+  console.log(myLodash.template(`Wow! so cool! <%= name %>`)({ name: 'Kurumi!!!' })); // Wow! so cool! Kurumi!!!
+})();
 </script>
 ```
 
+
+## Method
+### constructor(opt)
+#### opt - object
+  - `modules` - required -  key: package name, value: package url
+  - `order` - optional - the execution order of package
+     eg: the peerdependence of react-dom is react， order is ['react', 'react-dom']; 
+     default: `Object.keys(opt.module)`;
+  - `threadNum` - the maxium of xhr request, deafalt: `5`
+
+### install - install packages
+```js
+const kurumi = new Kurumi(...);
+kurumi.setThreadNum(1);
+kurumi.install();
+```
+
+### get(name) - name in Object.keys(opt.module)
+```js
+const kurumi = new Kurumi({
+  modules: {
+    'react': 'https://unpkg.com/react@16.8.4/umd/react.production.min.js',
+    'react-dom': 'https://unpkg.com/react-dom@16.8.4/umd/react-dom.production.min.js'
+  }
+});
+kurumi.install();
+kurumi.get('react').createElement('div', {}, 'this is a div!');
+```
+
+### setThreadNum(num) - set the maxium of xhr request 
+```js
+const kurumi = new Kurumi(...);
+kurumi.setThreadNum(1);
+kurumi.install();
+```
 ## FAQ
 ### Why is it called Kurumi?
 > `Kurumi` Tokisaki (時崎 狂三ときさき くるみ, Tokisaki Kurumii?) is the third Spirit to appear. Due to her brutal actions, she is referred to as the Worst Spirit (最悪の精霊, Saiaku no Seirei?). She is also the first Spirit to appear as an antagonist in the Date A Live series.
--- src/Kurumi.ts
+++ src/Kurumi.ts
@@ -1,39 +1,138 @@
-const downloadScript = async (url: string) => {
-  return await new Promise<string>((resolve) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onreadystatechange =  () => {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        resolve(xhr.responseText);
-      } else if (xhr.readyState == 4 && xhr.status !== 200) {
-        throw(new Error(`dowload failed: ${url}！！！`));
+export interface IMouldeCache {
+  [index: string]: ICache;
+}
+
+export interface ICache {
+  code: string;
+  fn?: any;
+}
+
+export interface IConfig {
+  modules: {
+    [index: string]: string;
+  };
+  // eval执行顺序，如果A依赖B，那么B必须位于A前面，即['B', 'A']
+  order?: string[];
+  threadNum?: number;
+}
+
+class Kurumi {
+  // cache
+  moduleCache: IMouldeCache = {};
+  config: IConfig;
+
+  // 是否处于预加载状态
+  isPrefetch = false;
+  prefetchQueue: string[] = []; // svg预加载队列
+
+  threadNum = 5;
+
+  constructor(config: IConfig) {
+    this.config = config;
+  }
+
+  private downloadSourceCode = async (url: string) => {
+    return await new Promise<string>((resolve) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.send();
+      xhr.onreadystatechange =  () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          resolve(xhr.responseText);
+        } else if (xhr.readyState == 4 && xhr.status !== 200) {
+          throw(new Error(`dowload failed: ${url}！！！`));
+        }
       }
+    })
+  };
+
+  private umd = (name: string, sourceCode: string) => {
+    const umd = `(function(){
+      var exports = {};
+      var module = {};
+      module.exports = {};
+      var require = function (name) {
+        const npmModule = that.moduleCache[name].fn;
+        if (npmModule) {
+          return npmModule;
+        } else {
+          throw(new Error('${name} depend on ' + name + ', please first load ${name}' ));
+        }
+      };
+    ` + sourceCode + `
+    return module.exports || exports.default;
+    })()`;
+    return umd;
+  };
+
+  private injectModule = async (name: string) => {
+    const url = this.config.modules[name];
+    const sourceCode = await this.downloadSourceCode(url);
+    const umdCode = this.umd(name, sourceCode);
+    this.moduleCache[name] = {code: umdCode};
+  }
+
+  private eval = (name: string) => {
+    const that = this;
+    const fn = eval(this.moduleCache[name].code);
+    this.moduleCache[name].fn = fn
+    // 释放内存
+    this.moduleCache[name].code = '';
+    return fn;
+  }
+
+  private getOrder = () => {
+    return this.config.order || Object.keys(this.config.modules);
+  }
+
+  // 开始安装  一般配置在onload后
+  install = async () => {
+    if (this.isPrefetch) {
+      throw(new Error('prefetching!'));
     }
-  })
-};
+    this.isPrefetch = true;
+    const keys = Object.keys(this.config.modules);
 
-const npmCache = {};
+    // 没有需要预加载的内容
+    if (keys.length === 0) {
+      this.isPrefetch = false;
+    } else {
+      this.prefetchQueue = keys;
+      await this.popQueue();
+    }
 
-const Kurumi = async (name: string, url: string) => {
-  const moduleContent = await downloadScript(url);
-  const script = `(function(){
-    var exports = {};
-    var module = {};
-    module.exports = {};
-    var require = function (name) {
-      const npmModule = npmCache[name];
-      if (npmModule) {
-        return npmModule;
-      } else {
-        throw(new Error('${name} depend on ' + name + ', please first load ${name}' ));
-      }
-    };
-  ` + moduleContent + `
-  npmCache[name] = module.exports || exports.default;
-  return npmCache[name];
-  })()`;
-  npmCache;
-  return eval(script);
-};
+    // source code -> fn
+    this.getOrder().forEach(this.eval);
+    return this;
+  }
+
+  // 出队
+  private popQueue = async () => {
+    if (this.prefetchQueue.length === 0) {
+      this.isPrefetch = false;
+      return;
+    }
+    const temp = this.prefetchQueue.splice(0, this.threadNum);
+    await Promise.all(temp.map(this.injectModule));
+    await this.popQueue();
+    return true;
+  }
+
+  get = (name: string) => {
+    if (!this.moduleCache[name]) {
+      throw(new Error(`no ${name} module! please install firstly!`))
+    }
+    const fn = this.moduleCache[name].fn;
+    if (fn) {
+      return fn;
+    }
+    this.injectModule(name);
+    return this.eval(name);
+  };
+
+  // 修改最大并发下载数
+  setThreadNum = (num: number) => {
+    this.threadNum = num;
+  };
+}
 export default Kurumi;
